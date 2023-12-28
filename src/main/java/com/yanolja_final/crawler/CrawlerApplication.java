22f1a5e6b46af07984d4c1db1efdb73e43e42dc8
deepredk <deepred.kim@gmail.com>
2023-12-28 10:07:40 +0900

    Initial commit

---
+++ src/main/java/com/yanolja_final/crawler/CrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.yanolja_final.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanolja_final/crawler/CrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanolja_final.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
