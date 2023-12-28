4eb08f057bb9b208254b341ca697c455c0b27133
deepredk <deepred.kim@gmail.com>
2023-12-28 13:32:06 +0900

    [test] ApiResponseFetcher 테스트 케이스 추가

--- src/test/java/com/yanolja_final/crawler/util/ApiResponseFetcherTest.java
+++ src/test/java/com/yanolja_final/crawler/util/ApiResponseFetcherTest.java
@@ -2,22 +2,50 @@ package com.yanolja_final.crawler.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ApiResponseFetcherTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("providedSourceForGet")
     @DisplayName("GET 메서드 요청에 대해 Http ResponseBody가 잘 들어온다")
-    void get() throws Exception {
-        // given
-        String url = "https://travel.interpark.com/api-package/goods/tripInfo/24030517228/18";
-
-        // when
+    void get(String url, String keyword) throws Exception {
         String responseBody = ApiResponseFetcher.get(url);
+        assertThat(responseBody).contains(keyword);
+    }
 
-        // then
-        assertThat(responseBody).contains("[베스트셀러] 괌 PIC 골드카드+전일자유일정 오전출발4일_티웨이항공");
+    private static Stream<Arguments> providedSourceForGet() {
+        return Stream.of(
+            Arguments.of(
+                "https://travel.interpark.com/api-package/goods/image/B4010646?pageNo=1&pageSize=30",
+                "B4010646_5_813.jpg"
+            ),
+            Arguments.of(
+                "https://travel.interpark.com/api-package/goods/reservationCount?baseGoodsCd=B4010646",
+                "B4010646"
+            ),
+            Arguments.of(
+                "https://travel.interpark.com/api-package/goods/schedule/24030517228/18",
+                "오전출발 3박4일 티웨이항공 괌 PIC 자유일정!"
+            ),
+            Arguments.of(
+                "https://travel.interpark.com/api-package/goods/review/B4010646?pageNo=1&pageSize=10",
+                "5세 쌍둥이아이들 데리고 처음한 해외여행이었습니다."
+            ),
+            Arguments.of(
+                "https://travel.interpark.com/api-package/goods/keyword/187/187/3",
+                "체크,법인,기업,기프트카드 제외"
+            ),
+            Arguments.of(
+                "https://travel.interpark.com/api-package/calendar/2023/12?resveCours=p&baseGoodsCD=B4010646",
+                "lowestOfMonth"
+            )
+        );
     }
 
     @Test
